f9482bf470d6ed9d8e11a83a64d6a4917c8b8b97
porkchop-jim <cstarr114@gmail.com>
2018-04-16 14:46:41 +0900

    Examples of variables and loops.

--- crytpoticker/main.py
+++ crytpoticker/main.py
@@ -1,12 +1,26 @@
 from tkinter import *
 
-root = Tk()
+# This is a comment and nothing happens.
 
-root.geometry('500x500')
 
-myLabel = Label(root, text="Some words.")
-myLabel.pack()
+myStatement='I got it'
 
-root.mainloop()
+
+x = 0
+while x < 10:
+    print(myStatement)
+    x = x + 2
+
+# root = Tk()
+#
+# root.geometry('400x500')
+#
+# myLabel1 = Label(root, text="Other words.")
+# myLabel1.pack()
+#
+# myLabel2 = Label(root, text="Other words.")
+# myLabel2.pack()
+#
+# root.mainloop()
 
 
