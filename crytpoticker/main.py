901aa0ca50501a805f3200555095a75044698a80
porkchop-jim <cstarr114@gmail.com>
2018-04-18 13:31:24 +0900

    Examples of functions.

--- crytpoticker/main.py
+++ crytpoticker/main.py
@@ -1,6 +1,19 @@
 from tkinter import *
-
+import webbrowser
 # This is a comment and nothing happens.
+#webbrowser.open("https://www.youtube.com/watch?v=nh-QyvRXlVg")
+def myfunction(btcPrice, btcVolume):
+    print(btcPrice*btcVolume)
+
+myfunction(10000, 4)
+myfunction(4, 0)
+myfunction(8 ,32)
+myfunction(77, 3)
+
+print(9*9)
+print(4*4)
+print(8*8)
+print(77*77)
 
 
 myStatement='I got it'
@@ -11,16 +24,16 @@ while x < 10:
     print(myStatement)
     x = x + 2
 
-# root = Tk()
-#
-# root.geometry('400x500')
-#
-# myLabel1 = Label(root, text="Other words.")
-# myLabel1.pack()
-#
-# myLabel2 = Label(root, text="Other words.")
-# myLabel2.pack()
-#
-# root.mainloop()
+root = Tk()
+
+root.geometry('400x500')
+
+myLabel1 = Label(root, text="Other words.")
+myLabel1.pack()
+
+myLabel2 = Label(root, text="Other words.")
+myLabel2.pack()
+
+root.mainloop()
 
 
